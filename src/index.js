e2247b3cb39edd5aea57d51fc2a0b4e40148c89d
Hakiz <NMT456456@gmail.com>
2023-09-09 13:10:54 +0700

    add check no internet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import fs from 'fs'
+import dns from 'dns'
+import cmd from 'node-cmd'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 import { checkIn, loginHSR } from './checkIn.js'
 
-const date = new Date()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
+const date = new Date()
 const today = date.getDate()
 const month = date.getMonth() + 1
 const year = date.getFullYear()
@@ -19,17 +24,19 @@ const addCheckedList = () => {
     )
 }
 
-const hsrAutoCheckIn = async () => {
-    const isSuccess = await checkIn()
-
-    if (isSuccess === true) {
-        addCheckedList()
-        console.log('Diem danh thanh cong')
-    } else if (isSuccess === false) {
-        await loginHSR()
+dns.resolve('www.google.com', async (err) => {
+    if (err) {
+        console.log('Not internet')
+        cmd.runSync(`start "" "${__dirname}/no_internet.html"`)
     } else {
-        console.log(isSuccess)
+        const isSuccess = await checkIn()
+        if (isSuccess === true) {
+            addCheckedList()
+            console.log('Diem danh thanh cong')
+        } else if (isSuccess === false) {
+            await loginHSR()
+        } else {
+            console.log(isSuccess)
+        }
     }
-}
-
-hsrAutoCheckIn()
+})
